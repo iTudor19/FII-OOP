a905d4844f2d6bb3744c75f5f8f906619c41d425
iTudor19 <tudor.iftene@yahoo.com>
2022-02-26 19:50:39 +0200

    problema 2 finalizata

--- lab 1-2.cpp
+++ lab 1-2.cpp
@@ -1,6 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+char a[1001];
+long long s;
+int conversion(char x[])
+{
+    int i = 0, s = 0;
+    while (x[i] != '\0' && x[i] != '\n')
+    {
+        s = s * 10 + (x[i] - '0');
+        i++;
+    }
+
+    return s;
+}
 int main()
 {
-    cout<<"hi";
+    FILE *f = fopen("in.txt", "r");
+    while (fgets(a, 1001, f) != NULL)
+        s = s + conversion(a);
+    fclose(f);
+    std::cout << s;
+    return 0;
 }
